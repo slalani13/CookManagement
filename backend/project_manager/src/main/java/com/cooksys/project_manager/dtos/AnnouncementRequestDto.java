59a7031b9e9cb37b5d39e72c2a5f77f5d66b83c6
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2025-02-25 15:21:40 -0500

    added both dto and id for announcementDto till I can test

--- backend/project_manager/src/main/java/com/cooksys/project_manager/dtos/AnnouncementRequestDto.java
+++ backend/project_manager/src/main/java/com/cooksys/project_manager/dtos/AnnouncementRequestDto.java
@@ -12,6 +12,9 @@ public class AnnouncementRequestDto {
     private Timestamp date;
     private String title;
     private String message;
+//    still unsure which way to proceed with dto or id
+    private Long companyId;
+    private Long authorId;
     private BasicUserDto author;
 
 }
