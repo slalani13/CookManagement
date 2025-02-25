05ae908b840886a25397b625ea1d621ae0e25ba8
rishijs <rishijs2002@gmail.com>
2025-02-25 10:59:02 -0500

    add deleted to controllers

--- backend/project_manager/src/main/java/com/cooksys/project_manager/controllers/TeamController.java
+++ backend/project_manager/src/main/java/com/cooksys/project_manager/controllers/TeamController.java
@@ -2,6 +2,7 @@ package com.cooksys.project_manager.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,5 +42,12 @@ public class TeamController {
     public TeamResponseDto updateTeam() {
         return null;
     }
+
+    @DeleteMapping
+    public TeamResponseDto deleteTeam() {
+        return null;
+    }
+
+
     
 }
--- backend/project_manager/src/main/java/com/cooksys/project_manager/controllers/UserController.java
+++ backend/project_manager/src/main/java/com/cooksys/project_manager/controllers/UserController.java
@@ -1,10 +1,16 @@
 package com.cooksys.project_manager.controllers;
 
-import com.cooksys.project_manager.dtos.UserRequestDto;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.cooksys.project_manager.dtos.AnnouncementResponseDto;
+import com.cooksys.project_manager.dtos.UserRequestDto;
 import com.cooksys.project_manager.dtos.UserResponseDto;
 import com.cooksys.project_manager.services.UserService;
 
@@ -37,6 +43,11 @@ public class UserController {
 
     @PatchMapping("/update")
     public UserResponseDto updateProfile() {
+        return null;
+    }
+
+    @DeleteMapping
+    public UserResponseDto deactivateUser() {
         return null;
     }
 }
