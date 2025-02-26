04aae7e310b2616d78b9dd10063b8c2a15218f3a
rishijs <rishijs2002@gmail.com>
2025-02-26 16:55:01 -0500

    project component initialization

---
+++ frontend/CookSysDirectoryFrontEnd/src/app/project-component/project-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-component',
+  imports: [],
+  templateUrl: './project-component.component.html',
+  styleUrl: './project-component.component.css'
+})
+export class ProjectComponentComponent {
+
+}
