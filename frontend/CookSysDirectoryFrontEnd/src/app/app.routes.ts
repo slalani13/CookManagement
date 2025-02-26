3312b32f26b8b5e63674f60ad7904a685856c31d
rishijs <rishijs2002@gmail.com>
2025-02-26 16:45:15 -0500

    update routes

--- frontend/CookSysDirectoryFrontEnd/src/app/app.routes.ts
+++ frontend/CookSysDirectoryFrontEnd/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { SelectCompanyComponent } from './select-company/select-company.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { TeamComponent } from './team/team.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'select-company', component: SelectCompanyComponent },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'team', component: TeamComponent },
+    { path: 'projects', component: ProjectsComponent }
 ];
