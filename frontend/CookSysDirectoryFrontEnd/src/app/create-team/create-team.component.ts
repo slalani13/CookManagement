bad2bde3d45a8e806a0fb9b4530565cf558822cb
Gabriel <119620067+GQ-code@users.noreply.github.com>
2025-02-25 22:51:45 -0800

    Initialized necessary components for frontend

---
+++ frontend/CookSysDirectoryFrontEnd/src/app/create-team/create-team.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-team',
+  imports: [],
+  templateUrl: './create-team.component.html',
+  styleUrl: './create-team.component.css'
+})
+export class CreateTeamComponent {
+
+}
---
+++ frontend/CookSysDirectoryFrontEnd/src/app/users-reg/users-reg.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-reg',
+  imports: [],
+  templateUrl: './users-reg.component.html',
+  styleUrl: './users-reg.component.css'
+})
+export class UsersRegComponent {
+
+}
