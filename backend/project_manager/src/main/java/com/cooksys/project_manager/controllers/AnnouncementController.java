bcc2790080b9b256181be5be505da741689fb96d
rishijs <rishijs2002@gmail.com>
2025-02-27 17:42:08 -0500

    removed announcement extra methods

--- backend/project_manager/src/main/java/com/cooksys/project_manager/controllers/AnnouncementController.java
+++ backend/project_manager/src/main/java/com/cooksys/project_manager/controllers/AnnouncementController.java
@@ -2,9 +2,14 @@ package com.cooksys.project_manager.controllers;
 
 import java.util.List;
 
-import com.cooksys.project_manager.dtos.AnnouncementRequestDto;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.cooksys.project_manager.dtos.AnnouncementRequestDto;
 import com.cooksys.project_manager.dtos.AnnouncementResponseDto;
 import com.cooksys.project_manager.services.AnnouncementService;
 
@@ -23,24 +28,9 @@ public class AnnouncementController {
         return announcementService.getAnnouncementsByCompanyId(id);
     }
 
-    @GetMapping
-    public AnnouncementResponseDto getAnnouncement() {
-        return null;
-    }
-
     @PostMapping
     public AnnouncementResponseDto createAnnouncement(@RequestBody AnnouncementRequestDto announcementRequestDto) {
         return announcementService.createAnnouncement(announcementRequestDto);
-    }
-
-    @PatchMapping("/announcement/update")
-    public AnnouncementResponseDto updateAnnouncement() {
-        return null;
-    }
-    
-    @DeleteMapping
-    public AnnouncementResponseDto deleteAnnouncement() {
-        return null;
     }
 
 }
